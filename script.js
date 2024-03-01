77b017d3ccad04a37ad97c092dbe644e5c98c3c1
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-03-01 14:30:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ function changeItem() {
   });
 }
 
-//add button and quantity for each, button to add new. do nice design
+function nightMode() {
+  var body = document.body;
+  body.classList.toggle('nightMode');
+}
+//add button and quantity (with options kg/Stk/packs) for each, button to add new. do nice design
+//add tags(specific shop, or type of shop, z. B. cosmetic shop or pharmacy)
+//each item has a crossout button, quantity, edit/delete option and option of tags to it. can create new items
+//crossed out items "sink", going after non-crossed items, on top of other crossed items
+//tags for "buy asap/not urgent/can wait for a long time"
