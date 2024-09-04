618c25d858a451d1d3929e86905111d775e8c0c3
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-09-04 21:28:38 +0200

    Update and rename server.js to server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+esponse = requests.get(
+  'url here')
+for answers in response.json()['items']:
+  print(data[’title’])
+  print(data[’title’])
+  print(data[’title’])
