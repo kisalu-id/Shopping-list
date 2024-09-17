b04cab46252714133dc09b4e7800dcfb24861c39
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-09-17 16:43:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+import requests
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)   #initialize the flask app#
 
-app = Flask(__name__)   #initialize the flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'   #configure the app to use SQLite database sfrom data.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/username/Desktop/folder/Shopping list/shopping_list2.db'   #configure the app to use SQLite database sfrom data.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/DDXNB_Alien15/Desktop/Entwicklung/Shopping list/shopping_list2.db'   #configure the app to use SQLite database sfrom data.db
 
 db = SQLAlchemy(app)   #binding
 
@@ -24,7 +27,6 @@ def repr(self):   #provides a representation of the object when printed
     return f"{self.name} {self.quantity}"
 
 @app.route('/groceries', methods=['GET'])   #route where the list of items will be
-
 def get_groceries():
     groceries = Item.query.all()
     items = []
@@ -37,19 +39,50 @@ def get_groceries():
             'by_sina': item.by_sina,
             'emoji_id': item.emoji_id
         })
-    return {items}
+    return {'groceries': items}
 
 
 
-@app.route('/groceries', methods=[''])
+@app.route('/groceries', methods=['POST'])   #adding a new grocery item
 def add_grocery():
+    id =       #add new unique id, how?
+    name =
+    quantity =
+    crossed_out =
+    by_sina =
+    emoji_id =
+
+    if not name:
+        alert("Invalid input")
 
 
+    new_item = Item(
+        id = id,
+        name=name,
+        quantity=quantity,
+        crossed_out=crossed_out,
+        by_sina=by_sina,
+        emoji_id=emoji_id
+    )
 
-@app.route('/groceries', methods=[''])
+
+@app.route('/groceries', methods=['PUT'])   #updating an existing item
 def update_grocery(id):
+    item_to_update =
+
+    if not item_to_update:
+        alert("Error")
 
 
 
-@app.route('/groceries', methods=[''])
+@app.route('/groceries', methods=['DELETE'])
 def delete_grocery(id):
+    item_to_delete =
+
+    if not item_to_delete:
+        alert("Error")
+
+
+
+if __name__ == '__main__':
+    main()    #???
